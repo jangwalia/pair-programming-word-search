692a67b00e72de9e9f39a1fdf0b87e6ae1164d9b
jang walia <jangiwalia83@gmail.com>
2021-11-17 16:28:51 -0800

    submitting pair programme for word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
 const wordSearch = (letters, word) => { 
+    let vertical = []
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
+    for (let i = 0; i < letters.length; i++) {
+        let vTemp = []
+        for (let j = 0; j < letters[i].length; j++) {
+            
+            const first = letters[j][i];
+            vTemp.push(first)
+        }
+        let x = vTemp.join('')
+        vertical = vertical.concat(x)
+    }
+    const allJoin = horizontalJoin.concat(vertical)
+    console.log(allJoin);
+    for (l of allJoin) {
+        // if (l === word) return true
         if (l.includes(word)) return true
     }
-}
-
-module.exports = wordSearch
+  
+    return false
+  }
+  module.exports = wordSearch
